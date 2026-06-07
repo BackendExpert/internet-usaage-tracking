39fc993251ad666672f1e5a0cd2a8cc5c4097317
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2026-06-07 08:00:39 +0530

    Update auth.controller.ts

--- internet-bk/src/auth/auth.controller.ts
+++ internet-bk/src/auth/auth.controller.ts
@@ -4,6 +4,7 @@ import { ClientInfoDecorator } from "src/common/decorators/client-info.decorator
 import type { ClientInfo } from "src/common/interfaces/client-info.interface";
 import { RegistationDTO } from "./dto/register.dto";
 import { LoginDto } from "./dto/login.dto";
+import { PasswordRestDTO } from "./dto/passreset.dto";
 
 
 @Controller('api/auth')
@@ -32,6 +33,32 @@ export class AuthController {
         @ClientInfoDecorator() client: ClientInfo,
     ) {
         return this.authService.Login(
+            dto,
+            client.ipAddress,
+            client.userAgent
+        )
+    }
+
+    @Post("/password-reset-request")
+    PasswordResetReqeust(
+        @Body('email') email: string,
+        @ClientInfoDecorator() client: ClientInfo,
+    ) {
+        return this.authService.RequestPasswordRest(
+            email,
+            client.ipAddress,
+            client.userAgent
+        )
+    }
+
+    @Get("verify-password-reset-request")
+    VerifyPasswordRest(
+        @Query("token") token: string,
+        dto: PasswordRestDTO,
+        @ClientInfoDecorator() client: ClientInfo,
+    ) {
+        return this.authService.VerifyPasswordReset(
+            token,
             dto,
             client.ipAddress,
             client.userAgent
